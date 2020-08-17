0072f0077fab895e06825106ca888a5abdaeff24
bigsaigon333 <likepeppermint@gmail.com>
2020-08-17 23:56:47 +0900

    add middlewares

--- index.js
+++ index.js
@@ -1,9 +1,33 @@
-const express = require("express");
+import express from "express";
+import morgan from "morgan";
+import helmet from "helmet";
+import cookieParser from "cookie-parser";
+import bodyParser from "body-parser";
 
 const app = express();
+const PORT = 4000;
 
-app.get("/", (req, res) => {
+const handleHome = (req, res) => {
 	res.send("Hello world!");
-});
+};
 
-app.listen(4000);
+const handleProfile = (req, res) => {
+	res.send("You're on my profile!");
+};
+const handleListening = () => {
+	console.log(`Listening on: http://localhost:${PORT}`);
+};
+const middlewareTest = (req, res, next) => {
+	console.log("[Log]: middleware is working");
+	next();
+};
+
+// app.use(middlewareTest);
+app.use(helmet());
+app.use(cookieParser());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(morgan("dev"));
+app.get("/", handleHome);
+app.get("/profile", handleProfile);
+app.listen(PORT, handleListening);
