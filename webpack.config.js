ee71e758afeb00a8c3108b5fb87ccabc6d89eb38
bigsaigon333 <likepeppermint@gmail.com>
2020-08-31 23:54:50 +0900

    install webpack

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const MODE = process.env.WEBPACK_ENV;
+const ENTRY_FILE = path.resolve(__dirname, "assets", "js", "main.js");
+const OUTPUT_DIR = path.join(__dirname, "static");
+
+const config = {
+	entry: ENTRY_FILE,
+	mode: MODE,
+	plugins: [new MiniCssExtractPlugin()],
+	module: {
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					MiniCssExtractPlugin.loader,
+					"css-loader",
+					"postcss-loader",
+					"scss-loader",
+				],
+			},
+		],
+	},
+	output: { path: OUTPUT_DIR, filename: "[name].js" },
+};
+
+module.exports = config;
