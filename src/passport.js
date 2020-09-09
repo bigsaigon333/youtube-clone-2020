b62d72388161bc8476020a92709a970ceed1ea8f
bigsaigon333 <likepeppermint@gmail.com>
2020-09-09 22:51:56 +0900

    .

--- src/passport.js
+++ src/passport.js
@@ -13,7 +13,7 @@ passport.use(
 		{
 			clientID: process.env.GITHUB_CLIENT_ID,
 			clientSecret: process.env.GITHUB_CLIENT_SECRET,
-			callbackURL: `http://localhost:4000${routes.githubCallback}`,
+			callbackURL: `https://powerful-ravine-68132.herokuapp.com${routes.githubCallback}`,
 			scope: "user:email",
 		},
 		githubLoginCallback
